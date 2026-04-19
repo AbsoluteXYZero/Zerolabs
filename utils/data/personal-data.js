554bbe935edadea94aca1f7e11d5ec4ef91624d1
Zero <183767710+AbsoluteXYZero@users.noreply.github.com>
2026-04-19 01:37:43 -0400

    Update personalData description to include 'Scientist'

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "AbsoluteXYZero",
   profile: '/profile.png',
   designation: "Indie Software Developer",
-  description: "I make software to solve problems I personally encounter because no one else will",
+  description: "Scientist by day, code monkey at night. I make software to solve problems I personally encounter because no one else will",
   email: 'support@zerolabs.fyi',
   gitlab: 'https://gitlab.com/users/AbsoluteXYZero/projects',
 }
